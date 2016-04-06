cdb516e7a40296cb035efd31fb629afe7a375ede
jhyeon1010 <jhyeon1010@naver.com>
2016-04-06 18:14:43 +0900

    환경정보 수집 안내 내용 표시

--- views/setting.blade.php
+++ views/setting.blade.php
@@ -11,10 +11,21 @@
             <input type="hidden" name="_token" value="{{ csrf_token() }}">
 
             <div class="form-group">
-                <label>정보 수집 동의</label>
+                <div class="well">
+                    <h4>XE가 설치된 서버의 환경정보 일부를 디버깅 목적으로 수집하며, 이 과정에서 개인정보는 수집되지 않습니다.</h4>
+                    <br />
+                    <br />
+                    <strong>수집하는 항목:</strong><br />
+                    * 서버 OS<br />
+                    * http software (apache, nginx)<br />
+                    * php version 및 extension 목록<br />
+                    * database 종류 및 버전<br />
+                    * 설치된 플러그인 정보<br />
+                </div>
                 <div class="checkbox">
                     <label>
-                        <input type="checkbox" name="agree" value="true" @if($agree) checked @endif> Agree
+                        <input type="checkbox" name="agree" value="true" @if($agree) checked @endif>
+                        <strong>시스템 환경정보 수집에 동의합니다.</strong>
                     </label>
                 </div>
             </div>
